65d8b01aa733d9416a323f324f02ecfa97c55e67
Makoto Hamanaka <m-hamanaka@tonchidot.com>
2013-05-13 15:41:05 +0900

    modify UiLifecycleHelper that it can use any SessionFactory to create new Session instances.

--- facebook/src/com/facebook/UiLifecycleHelper.java
+++ facebook/src/com/facebook/UiLifecycleHelper.java
@@ -45,6 +45,10 @@ public class UiLifecycleHelper {
     private static final String DIALOG_CALL_BUNDLE_SAVE_KEY =
             "com.facebook.UiLifecycleHelper.pendingFacebookDialogCallKey";
 
+    public interface SessionFactory {
+        public Session create();
+    }
+
     private final static String ACTIVITY_NULL_MESSAGE = "activity cannot be null";
 
     private final Activity activity;
@@ -54,6 +58,7 @@ public class UiLifecycleHelper {
     // Members related to handling FacebookDialog calls
     private FacebookDialog.PendingCall pendingFacebookDialogCall;
     private AppEventsLogger appEventsLogger;
+    private final SessionFactory sessionFactory;
 
     /**
      * Creates a new UiLifecycleHelper.
@@ -70,6 +75,26 @@ public class UiLifecycleHelper {
         this.callback = callback;
         this.receiver = new ActiveSessionBroadcastReceiver();
         this.broadcastManager = LocalBroadcastManager.getInstance(activity);
+        this.sessionFactory = null;
+    }
+
+    /**
+     * Creates a new UiLifecycleHelper.
+     *
+     * @param activity the Activity associated with the helper. If calling from a Fragment,
+     *                 use {@link android.support.v4.app.Fragment#getActivity()}
+     * @param callback the callback for Session status changes, can be null
+     * @param factory  the factory object to create new session instance.
+     */
+    public UiLifecycleHelper(Activity activity, Session.StatusCallback callback, SessionFactory factory) {
+        if (activity == null) {
+            throw new IllegalArgumentException(ACTIVITY_NULL_MESSAGE);
+        }
+        this.activity = activity;
+        this.callback = callback;
+        this.receiver = new ActiveSessionBroadcastReceiver();
+        this.broadcastManager = LocalBroadcastManager.getInstance(activity);
+        this.sessionFactory = factory;
     }
 
     /**
@@ -84,7 +109,10 @@ public class UiLifecycleHelper {
                 session = Session.restoreSession(activity, null, callback, savedInstanceState);
             }
             if (session == null) {
-                session = new Session(activity);
+                if (this.sessionFactory != null)
+                    session = this.sessionFactory.create();
+                else
+                    session = new Session(activity);
             }
             Session.setActiveSession(session);
         }
